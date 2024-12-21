54d83090d706064cd604973e90562683fa0519fa
Jasmin Patel <jasmin.patel2110@gmail.com>
2024-12-21 18:20:33 +0530

    Revert "home path to index path for deployment testing"
    This reverts commit 82ef578ec3573447214042ac53eb6da5fc616504.

--- hms/hms/urls.py
+++ hms/hms/urls.py
@@ -20,7 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('users.urls')),
     
     path('users/', include('users.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
--- hms/users/urls.py
+++ hms/users/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('index/', views.home, name='index'),
-    path('/', views.home, name='index'),
 ]
